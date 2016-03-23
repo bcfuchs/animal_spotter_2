c238183fd71a79009c146f189b7bef51d76dd14f
Brian <bcfuchs@gmail.com>
2016-03-23 11:39:13 +0000

    1st 2 pages

---
+++ www/js/anspot.js
@@ -0,0 +1,64 @@
+! function(){
+
+document.addEventListener("deviceready",anspot,false);
+    var hover_species,save_species_select;
+    function anspot() {
+	console.log("animal spotter !");
+	click_species();
+	save_species_select();
+	
+    }
+
+    save_species_select = function() {
+	var storeWrapper,speciesType;
+
+	$(document).ready(function(){
+	    console.log($(".sight-link"));
+	    $(".sight-link").on('click',function(e){
+
+		var species = $(this).attr('data-animal-type');
+		storeWrapper('anspot',species);
+	    });
+	});
+	
+	var storeWrapper = function(name, speciesType) {
+	    var store = new Lawnchair({name:name}, function(store) {
+	    var key = "species-type";
+	    // Create an object
+	    var me = {key:key,value:speciesType}; // from scope
+
+	    // Save it
+	    store.save(me);
+
+	    // Access it later... Yes even after a page refresh!
+	    store.get(key, function(me) {
+		console.log(me);
+	    });
+	});
+	}
+
+
+    }
+    click_species = function(){
+	console.log('setting up click species !');
+	$(".species-select").click(function(){
+	    console.log("clicked specsel");
+	    $(".query-panel-img").addClass('bw')
+	    $(this).find(".query-panel-img").removeClass('bw')
+	    
+
+	});
+    }
+
+    var storage_get = function(name,key,cb){
+	Lawnchair({name:name},function(){
+	    this.get('species-type', function(d) {
+		cb(d);
+	    })
+	})
+
+    }
+
+    window.storage_get = storage_get
+
+}()
