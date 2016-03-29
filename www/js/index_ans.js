ab0e6e1386d2344221b42d895cfefc5ddd0971a4
Brian <bcfuchs@gmail.com>
2016-03-29 22:49:21 +0100

    adding file api

--- www/js/index_ans.js
+++ www/js/index_ans.js
@@ -1,5 +1,6 @@
 ! function() {
-    $(document).on('deviceready',function(){
+    document.addEventListener("deviceready", function() {
+//    $(document).on('deviceready',function(){
 	var save_species_select = function() {
 	    var log = function(m){ console.log(m)};
 	    
@@ -11,8 +12,62 @@
 	    
 	    
 	}
-	
+	var test_db = function(){
+
+	    function onInitFs(fs) {
+		console.log('Opened file system: ' + fs.name);
+	    }
+	    errorHandler = function(e) {
+		console.log(e);
+
+	    }
+	    window.requestFileSystem(LocalFileSystem.PERSISTENT, 50*1024*1024 /*50MB*/, onInitFs, errorHandler);
+	}
+
+	    var success = function(fs) {
+		console.log('Opened file system: ' + fs.name);
+		
+	    }
+	    var error = function(e) {
+		console.log(e);
+
+	    }
+
+//	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, success, error);
+	var test_write = function() {
+	    function writeToFile(fileName, data,type) {
+		data = JSON.stringify(data, null, '\t');
+		
+		window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+		    directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+			fileEntry.createWriter(function (fileWriter) {
+			    fileWriter.onwriteend = function (e) {
+				
+				// for real-world usage, you might consider passing a success callback
+				console.log('Write of file "' + fileName + '"" completed.');
+			    };
+
+			    fileWriter.onerror = function (e) {
+				// you could hook this up with our global error handler, or pass in an error callback
+				console.log('Write failed: ' + e.toString());
+			    };
+			    // NOTE -- image here. 
+			    var blob = new Blob([data], { type: type });
+			    fileWriter.write(blob);
+			}, errorHandler.bind(null, fileName));
+		    }, errorHandler.bind(null, fileName));
+		}, errorHandler.bind(null, fileName));
+	    }
+
+	    writeToFile('example.json', { foo: 'bar' });
+
+	}
+
+
+
 	save_species_select();
-    });
+	test_db();
+	test_write();
+    },false);
 
 }()
--- www/js/photo.js
+++ www/js/photo.js
@@ -1,105 +1,99 @@
+! function() {
+    var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value
 
-var pictureSource;   // picture source
-var destinationType; // sets the format of returned value
+    document.addEventListener("deviceready",onDeviceReady,false);
 
-// Wait for device API libraries to load
-//
-document.addEventListener("deviceready",onDeviceReady,false);
 
-// device APIs are available
-//
-function onDeviceReady() {
-    pictureSource=navigator.camera.PictureSourceType;
-    destinationType=navigator.camera.DestinationType;
-}
+    function onDeviceReady() {
+	pictureSource=navigator.camera.PictureSourceType;
+	destinationType=navigator.camera.DestinationType;
+    }
 
-// Called when a photo is successfully retrieved
-//
-function onPhotoDataSuccess(imageData) {
-    // Uncomment to view the base64-encoded image data
-    // console.log(imageData);
-
-    // Get image handle
+    // Called when a photo is successfully retrieved
     //
-    var div = document.createElement("div");
-    var a = document.createElement("a");
-    var id =  "id-"+(new Date()).getTime();
-    a.href = "form.html#"+id;
-    div.className = "gallery-image-container";
-    var smallImage = document.createElement("img");
-    //    document.getElementById('smallImage');
+    function onPhotoDataSuccess(imageData) {
+	// Uncomment to view the base64-encoded image data
+	// console.log(imageData);
 
-        
-    smallImage.className = "smallImage";
-    // Show the captured photo
-    // The in-line CSS rules are used to resize the image
-    //
-    smallImage.src = "data:image/jpeg;base64," + imageData;
-    saveData(imageData,id,function(r) {console.log("got an image!")});
-    var gall =     document.getElementById('image-gallery');
-    a.appendChild(smallImage);
-    div.appendChild(a);
-    gall.appendChild(div);
+	// Get image handle
+	//
+
+	var div = document.createElement("div");
+	var a = document.createElement("a");
+	var id =  "id-"+(new Date()).getTime();
+	a.href = "form.html#"+id;
+	div.className = "gallery-image-container";
+	var smallImage = document.createElement("img");
+	//    document.getElementById('smallImage');
+
+	
+	smallImage.className = "smallImage";
+	// Show the captured photo
+	// The in-line CSS rules are used to resize the image
+	//
+	smallImage.src = "data:image/jpeg;base64," + imageData;
+	saveData(imageData,id,function(r) {console.log("got an image!")});
+	var gall =     document.getElementById('image-gallery');
+	a.appendChild(smallImage);
+	div.appendChild(a);
+	gall.appendChild(div);
+	
+    }
+
+    function saveData(d,id,cb) {
+	// TODO store in a separate collection -- there might be more than one
+	storage_put('anspot',{key:'photo',value:d},cb);
+
+    }
     
-}
+    function saveDataURI(uri,cb) {
 
-function saveData(d,id,cb) {
-    // TODO store in a separate collection -- there might be more than one
-    storage_put('anspot',{key:'photo',value:d},cb);
-    /**    
-    Lawnchair(function(){
+	// TODO store in a separate collection -- there might be more than one
+	storage_put('anspot',{key:'photo',value:uri},cb);
+	    
+    }
+    
+    
+    function onPhotoURISuccess(imageURI) {
 
-	    this.save({image:d,id:id,key:id},cb);
-	});
-*/
-}
-// Called when a photo is successfully retrieved
-//
-function onPhotoURISuccess(imageURI) {
-    // Uncomment to view the image file URI
-    // console.log(imageURI);
+	console.log(imageURI);
+	saveDataURI(uri,function(m){ console.log("saving image data " + m);
+	$('#image-gallery').append($('<img></img>').attr('src',imageURI)).addClass("thumbnail");;
+	$('#cam-icon').style({'width','100px'});
 
-    // Get image handle
+    }
+
+    // A button will call this function
     //
-    var largeImage = document.getElementById('largeImage');
+    function capturePhoto() {
+	// Take picture using device camera and retrieve image as base64-encoded string
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+								  destinationType: destinationType.DATA_URL });
+    }
 
-    // Unhide image elements
+    // A button will call this function
     //
-    largeImage.style.display = 'block';
+    function capturePhotoEdit() {
+	// Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+								  destinationType: destinationType.DATA_URL });
+    }
 
-    // Show the captured photo
-    // The in-line CSS rules are used to resize the image
+    // A button will call this function
     //
-    largeImage.src = imageURI;
-}
+    function getPhotoURI(source) {
+	// Retrieve image file location from specified source
+	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+								 destinationType: destinationType.FILE_URI,
+								 sourceType: source });
+    }
 
-// A button will call this function
-//
-function capturePhoto() {
-    // Take picture using device camera and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-		destinationType: destinationType.DATA_URL });
-}
+    // Called if something bad happens.
+    //
+    function onFail(message) {
+	console.log('Failed because: ' + message);
+    }
 
-// A button will call this function
-//
-function capturePhotoEdit() {
-    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-		destinationType: destinationType.DATA_URL });
-}
 
-// A button will call this function
-//
-function getPhoto(source) {
-    // Retrieve image file location from specified source
-    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-		destinationType: destinationType.FILE_URI,
-		sourceType: source });
-}
-
-// Called if something bad happens.
-//
-function onFail(message) {
-    alert('Failed because: ' + message);
-}
+}();
